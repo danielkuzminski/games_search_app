0173a6c1a3061c9d798c2ffb79e823eebbc06c6f
danielkuzminski <dankuzmina@gmail.com>
2022-01-23 02:21:58 +0100

    add cookie consent to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import './App.css';
-import Search from './components/Search'
+import Search from './components/Search';
 import ScrollBar from './components/ScrollBar';
 import ToTopButton from './components/ToTopButton';
 import NewGameForm from './components/NewGameForm';
 import Hamburger from './components/Hamburger';
+import CookieConsent from 'react-cookie-consent';
 import { useState } from 'react';
 
 function App() {
-  
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   const handleShow = () => {
-    setShow(!show)
-  }
+    setShow(!show);
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <ScrollBar />
-      <Hamburger handleShow={handleShow}/>
+      <Hamburger handleShow={handleShow} />
       {show && <NewGameForm handleShow={handleShow} />}
       <Search />
       <ToTopButton />
+      <CookieConsent>
+        This website uses cookies to enhance the user experience. To find out more, please read our terms and policy
+      </CookieConsent>
     </div>
   );
 }
